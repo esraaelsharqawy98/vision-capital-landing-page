d8915150a45b880256e661e791f6ba92daa543f5
esraaelsharqawy98 <esraaelsharqawy998@gmail.com>
2024-06-24 01:37:54 +0300

    edit project

--- js/main.js
+++ js/main.js
@@ -1,24 +1,91 @@
 const form = document.getElementById("form");
-const project_name = document.querySelector("#project-name");
-const project_description = document.querySelector("#project-description");
-const telegram_name = document.querySelector("#telegram-name");
-const email = document.querySelector("#email");
-const error = document.querySelector(".invalid-feedback");
+const project_name = document.getElementById("project-name");
+const project_description = document.getElementById("project-description");
+const telegram_name = document.getElementById("telegram-name");
+const email = document.getElementById("email");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
   checkInputs();
 });
 
+
+// Checks the input values of the form and sets error or success states accordingly.
 function checkInputs() {
-  let projectNameValue = project_name.value().trim();
-  let projectDescriptionValue = project_description.value().trim();
-  let telegramNameValue = telegram_name.value().trim();
-  let emailValue = email.value().trim();
+  const projectNameValue = project_name.value.trim();
+  const projectDescriptionValue = project_description.value.trim();
+  const telegramNameValue = telegram_name.value.trim();
+  const emailValue = email.value.trim();
+
+  if (projectNameValue === ""){
+    setErrorFor(project_name, "project name is required");
+  }else if(!/^[a-zA-Z0-9-_]+$/.test(projectNameValue)){
+     setErrorFor(project_name,'Project name can only contain letters, numbers, underscores, and hyphens');
+  }else if(projectNameValue.length > 50 || projectNameValue.length < 3){
+    setErrorFor(project_name,'Project name should be 3-50 characters');
+  }
+  else {
+    setSuccessFor(project_name);
+  }
+
+  if (projectDescriptionValue === "") {
+    setErrorFor(project_description, "project description is required");
+  }else if(!/^[a-zA-Z0-9\s.,]+$/.test(projectDescriptionValue)){
+    setErrorFor(project_description,'invalid Project description');
+ }else if(project_description.length > 200 || project_description.length < 50){
+   setErrorFor(project_description,'Project description should be 3-200 characters');
+ } 
+  else {
+    setSuccessFor(project_description);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "email is required");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "email is not valid");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (telegramNameValue === "") {
+    setErrorFor(telegram_name, "telegram name is required");
+  }else if(!/^[a-zA-Z0-9\s]+$/.test(telegramNameValue)){
+    setErrorFor(telegram_name,'invalid telegram name');
+ }else if(telegramNameValue.length > 50 || telegramNameValue.length < 3){
+   setErrorFor(telegram_name,'telegram name should be 3-50 characters');
+ }
+   else {
+    setSuccessFor(telegram_name);
+  }
 
 }
 
-function setErrorFor(input,message){
-    
+// Sets an error message for an input field.
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const errorText = formControl.querySelector(".error");
+
+  errorText.innerText = message;
+  input.classList.add('is-invalid');
+  errorText.className = "error invalid-feedback d-block"
+}
+
+// Sets a success state for an input field.
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  const errorText = formControl.querySelector(".error");
+
+  input.classList.remove('is-invalid');
+  input.classList.add('is-valid');
+  errorText.className = "error invalid-feedback d-none"
+}
+
+
+
+// validate an email address 
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
